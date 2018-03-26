efc9ccd3d6fca1942f99e6044fab34c173dabb10
SuperFinder <zhaichaowu@126.com>
2018-03-26 14:44:46 +0800

    新增发卡机K710的demo

---
+++ 外设/K710Demo/K710_Demo/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace K710_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(52, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(573, 72);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "串口连接";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(492, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "断开";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(387, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(257, 31);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(98, 20);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(208, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "波特率:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(71, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(98, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "端口:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button15);
+            this.groupBox2.Controls.Add(this.button11);
+            this.groupBox2.Controls.Add(this.button12);
+            this.groupBox2.Controls.Add(this.button13);
+            this.groupBox2.Controls.Add(this.button14);
+            this.groupBox2.Controls.Add(this.button7);
+            this.groupBox2.Controls.Add(this.button8);
+            this.groupBox2.Controls.Add(this.button9);
+            this.groupBox2.Controls.Add(this.button10);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 90);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(664, 247);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "操作区域";
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(52, 197);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(139, 44);
+            this.button15.TabIndex = 12;
+            this.button15.Text = "读卡";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(487, 142);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(139, 44);
+            this.button11.TabIndex = 11;
+            this.button11.Text = "发卡到卡口外(FC0)";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(342, 142);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(139, 44);
+            this.button12.TabIndex = 10;
+            this.button12.Text = "发卡到取卡位置(FC4)";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(197, 142);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(139, 44);
+            this.button13.TabIndex = 9;
+            this.button13.Text = "发卡到读卡位置(FC7)";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(52, 142);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(139, 44);
+            this.button14.TabIndex = 8;
+            this.button14.Text = "获取版本(GV)";
+            this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(487, 86);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(139, 44);
+            this.button7.TabIndex = 7;
+            this.button7.Text = "发卡到传感器2(FC6)";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(342, 86);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(139, 44);
+            this.button8.TabIndex = 6;
+            this.button8.Text = "禁止蜂鸣(BD)";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(197, 86);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(139, 44);
+            this.button9.TabIndex = 5;
+            this.button9.Text = "蜂鸣器使能(BE)";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(52, 86);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(139, 44);
+            this.button10.TabIndex = 4;
+            this.button10.Text = "复位(RS)";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(487, 31);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(139, 44);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "收卡(CP)";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(342, 31);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(139, 44);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "查询(RF)";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(197, 31);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(139, 44);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "高级查询(AP)";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(52, 31);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 44);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "发卡到取卡位置(DC)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 360);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "命令状态:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(77, 357);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(661, 21);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 381);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button15;
+    }
+}
+
---
+++ 外设/K710Demo/K710_Demo/Form1.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace K710_Demo
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("D1000_DLL.dll")]
+        public static extern int D1000_CommOpen(int Port);
+
+        [DllImport("D1000_DLL.dll")]
+        public static extern int D1000_CommOpenWithBaud(int Port, int Baudate);
+
+        [DllImport("D1000_DLL.dll")]
+        public static extern int D1000_CommClose(int ComHandle);
+
+        [DllImport("D1000_DLL.dll")]
+        public static extern int D1000_GetDllVersion(int ComHandle, char[] strVersion);
+
+        [DllImport("D1000_DLL.dll")]
+        public static extern int D1000_GetSysVersion(int ComHandle, byte MacAddr, byte[] strVersion);
+
+        [DllImport("D1000_DLL.dll")]
+        public static extern int D1000_SendCmd(int ComHandle, byte MacAddr, byte[] p_Cmd, int len);
+
+        [DllImport("D1000_DLL.dll")]
+        public static extern int D1000_Query(int ComHandle, byte MacAddr, byte[] StateInfo);
+
+        [DllImport("D1000_DLL.dll")]
+        public static extern int D1000_SensorQuery(int ComHandle, byte MacAddr, byte[] StateInfo);
+
+        [DllImport("D1000_DLL.dll")]
+        public static extern int D1000_AutoTestMac(int ComHandle, byte MacAddr);
+
+        [DllImport("D1000_DLL.dll")]
+        public static extern int D1000_ReadRecycleCardNum(int ComHandle, byte MacAddr, byte[] szData);
+
+        [DllImport("D1000_DLL.dll")]
+        public static extern int D1000_SetCardNum(int ComHandle, byte MacAddr, int Num);
+
+        [DllImport("D1000_DLL.dll")]
+        public static extern int D1000_DisEnableCount(int ComHandle, byte MacAddr);
+
+        [DllImport("D1000_DLL.dll")]
+        public static extern int D1000_EnableCount(int ComHandle, byte MacAddr);
+
+        [DllImport("D1000_DLL.dll")]
+        public static extern int D1000_ReadCardNumber(int ComHandle, byte MacAddr, byte[] CardNumber);
+
+
+        byte MacAddress = 0;
+        int ComHandle = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string str = "";
+            int nRet;
+
+            for (int i = 1; i < 50; i++)
+            {
+                str = "COM" + i;
+                ComHandle = D1000_CommOpen(i);
+                if (ComHandle > 0)
+                {
+                    D1000_CommClose(ComHandle);
+                    comboBox1.Items.Add(str);
+                }
+            }
+            if (comboBox1.Items.Count > 0)
+            {
+                comboBox1.SelectedIndex = 0;
+            }
+
+            comboBox2.Items.Add("1200");
+            comboBox2.Items.Add("2400");
+            comboBox2.Items.Add("4800");
+            comboBox2.Items.Add("9600");
+            comboBox2.Items.Add("19200");
+            comboBox2.Items.Add("38400");
+            comboBox2.Items.Add("57600");
+            comboBox2.Items.Add("115200");
+            comboBox2.SelectedIndex = 3;
+        }
+
+        private string HexToString(byte[] by)
+        {
+            string str = "";
+
+            for (int i = 0; by[i] != '\0'; i++)
+            {
+                char ch;
+                ch = (char)by[i];
+                str += ch;
+            }
+            return str;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int nRet;
+            byte[] sendcmd = new byte[3];
+
+            sendcmd[0] = (byte)'D';
+            sendcmd[1] = (byte)'C';
+
+            nRet = D1000_SendCmd(ComHandle, MacAddress, sendcmd, 2);
+            if (nRet == 0)
+            {
+                textBox1.Text = "发卡到取卡位置成功";
+            }
+            else
+                textBox1.Text = "发卡到取卡位置失败";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int nRet=1;
+            byte[] Recv = new byte[200];
+
+            ComHandle = D1000_CommOpenWithBaud(int.Parse(comboBox1.Text.Substring(3, 1)), int.Parse(comboBox2.Text));
+            if(ComHandle > 0)
+            {
+                for (int i = 0; i < 16; i++)
+                {
+                    nRet = D1000_AutoTestMac(ComHandle, (byte)i);
+                    if (nRet == 0)
+                    {
+                        MacAddress = (byte)i;
+                        break;
+                    }
+                }
+
+                if(nRet==0)
+                {
+                    textBox1.Text = "设备连接成功";
+                    button1.Enabled = false;
+                    button2.Enabled = true;
+                }
+                else
+                {
+                    textBox1.Text = "设备连接失败";
+                }
+            }
+            else
+            {
+                textBox1.Text = "串口打开失败";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int nRet = D1000_CommClose(ComHandle);
+            if(nRet == 0)
+            {
+                button1.Enabled = true;
+                button2.Enabled = false;
+                textBox1.Text = "设备断开成功";
+            }
+            else
+                textBox1.Text = "设备断开失败";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int nRet;
+            byte[] sendcmd = new byte[3];
+
+            sendcmd[0] = (byte)'C';
+            sendcmd[1] = (byte)'P';
+
+            nRet = D1000_SendCmd(ComHandle, MacAddress, sendcmd, 2);
+            if (nRet == 0)
+            {
+                textBox1.Text = "回收成功";
+            }
+            else
+                textBox1.Text = "回收失败";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            int nRet;
+            byte[] sendcmd = new byte[3];
+
+            sendcmd[0] = (byte)'R';
+            sendcmd[1] = (byte)'S';
+
+            nRet = D1000_SendCmd(ComHandle, MacAddress, sendcmd, 2);
+            if (nRet == 0)
+            {
+                textBox1.Text = "复位成功";
+            }
+            else
+                textBox1.Text = "复位失败";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            int nRet;
+            byte[] sendcmd = new byte[3];
+
+            sendcmd[0] = (byte)'B';
+            sendcmd[1] = (byte)'E';
+
+            nRet = D1000_SendCmd(ComHandle, MacAddress, sendcmd, 2);
+            if (nRet == 0)
+            {
+                textBox1.Text = "允许蜂鸣设置成功";
+            }
+            else
+                textBox1.Text = "允许蜂鸣设置失败";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int nRet;
+            byte[] sendcmd = new byte[3];
+
+            sendcmd[0] = (byte)'B';
+            sendcmd[1] = (byte)'D';
+
+            nRet = D1000_SendCmd(ComHandle, MacAddress, sendcmd, 2);
+            if (nRet == 0)
+            {
+                textBox1.Text = "禁止蜂鸣设置成功";
+            }
+            else
+                textBox1.Text = "禁止蜂鸣设置失败";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int nRet;
+            byte[] sendcmd = new byte[3];
+
+            sendcmd[0] = (byte)'F';
+            sendcmd[1] = (byte)'C';
+            sendcmd[2] = (byte)'6';
+
+            nRet = D1000_SendCmd(ComHandle, MacAddress, sendcmd, 3);
+            if (nRet == 0)
+            {
+                textBox1.Text = "发卡到传感器2位置成功";
+            }
+            else
+                textBox1.Text = "发卡到传感器2位置失败";
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            int nRet;
+            byte[] _Version = new byte[50];
+
+            nRet = D1000_GetSysVersion(ComHandle, MacAddress, _Version);
+            if (nRet == 0)
+            {
+                MessageBox.Show(HexToString(_Version), "版本信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                textBox1.Text = "获取版本号成功";
+            }
+            else
+                textBox1.Text = "获取版本号失败";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            int nRet;
+            byte[] sendcmd = new byte[3];
+
+            sendcmd[0] = (byte)'F';
+            sendcmd[1] = (byte)'C';
+            sendcmd[2] = (byte)'7';
+
+            nRet = D1000_SendCmd(ComHandle, MacAddress, sendcmd, 3);
+            if (nRet == 0)
+            {
+                textBox1.Text = "发卡到读卡位置成功";
+            }
+            else
+                textBox1.Text = "发卡到读卡位置失败";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            int nRet;
+            byte[] sendcmd = new byte[3];
+
+            sendcmd[0] = (byte)'F';
+            sendcmd[1] = (byte)'C';
+            sendcmd[2] = (byte)'4';
+
+            nRet = D1000_SendCmd(ComHandle, MacAddress, sendcmd, 3);
+            if (nRet == 0)
+            {
+                textBox1.Text = "发卡到取卡位置成功";
+            }
+            else
+                textBox1.Text = "发卡到取卡位置失败";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            int nRet;
+            byte[] sendcmd = new byte[3];
+
+            sendcmd[0] = (byte)'F';
+            sendcmd[1] = (byte)'C';
+            sendcmd[2] = (byte)'0';
+
+            nRet = D1000_SendCmd(ComHandle, MacAddress, sendcmd, 3);
+            if (nRet == 0)
+            {
+                textBox1.Text = "发卡到卡口外成功";
+            }
+            else
+                textBox1.Text = "发卡到卡口外失败";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            byte[] cardNo = new byte[5];
+            string result = "";
+            if (D1000_ReadCardNumber(ComHandle, MacAddress, cardNo) == 0)
+            {
+                foreach (byte b in cardNo)
+                {
+                    result += b.ToString("x2");
+                }
+                textBox1.Text = "读卡成功:" + result;
+            }
+            else
+            {
+                textBox1.Text = "读卡失败";
+            }
+        }
+    }
+}
